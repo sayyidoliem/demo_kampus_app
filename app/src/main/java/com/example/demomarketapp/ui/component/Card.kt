9f6ba59f5a1e5b3aa7e2cc34a3dbe906a0c54e13
Sayyid Olim <sayyid.olim12@gmail.com>
2024-01-03 00:31:40 +0700

    Update CardListHomeDemo

--- app/src/main/java/com/example/demomarketapp/ui/component/Card.kt
+++ app/src/main/java/com/example/demomarketapp/ui/component/Card.kt
@@ -1,25 +1,43 @@
 package com.example.demomarketapp.ui.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddBox
+import androidx.compose.material.icons.filled.ArrowCircleRight
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Circle
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.outlined.AddCircleOutline
+import androidx.compose.material.icons.outlined.RemoveCircleOutline
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.demomarketapp.R
@@ -27,26 +45,63 @@ import com.example.demomarketapp.R
 @Preview
 @Composable
 fun prev() {
-    CardListCartDemo()
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CardListHomeDemo() {
-    Column() {
-        ListItem(
-            modifier = Modifier.clip(shape = RoundedCornerShape(20.dp)),
-            headlineText = { Text(text = "Judul") },
-            overlineText = { Text(text = "Alamat") },
-            trailingContent = {
-                Button(onClick = { /*TODO*/ }) {
+fun CardListHomeDemo(
+    product: String,
+    address: String,
+    ppp: String
+) {
+    Card(
+        modifier = Modifier
+            .clip(RoundedCornerShape(20.dp))
+            .padding(16.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        )
+    ) {
+        Column(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+            Box {
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .defaultMinSize(minWidth = 56.dp, minHeight = 56.dp),
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(8.dp),
+                    text = address
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier.padding(8.dp),
+                    text = "${product}\n${ppp}",
+                    fontWeight = FontWeight.SemiBold,
+                    style = MaterialTheme.typography.bodyLarge,
+                )
+                IconButton(modifier = Modifier,
+                    onClick = { /*TODO*/ }) {
                     Icon(
-                        imageVector = Icons.Default.ShoppingCart,
+                        imageVector = Icons.Default.ArrowCircleRight,
                         contentDescription = "Shopping Cart"
                     )
                 }
             }
-        )
+        }
     }
 }
 
@@ -60,29 +115,55 @@ fun CardListCartDemo() {
                 contentDescription = ""
             )
         },
-        headlineText = { Text(text = "Nma prodi") },
+        headlineText = { Text(text = "Product Name") },
         supportingText = {
             Column {
-                Row {
-                    Icon(imageVector = Icons.Default.Circle, contentDescription = "")
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        modifier = Modifier.size(12.dp),
+                        imageVector = Icons.Default.Circle,
+                        contentDescription = ""
+                    )
                     Spacer(modifier = Modifier.padding(8.dp))
-                    Text(text = "TEmpat")
+                    Text(text = "Vendor Name")
                 }
-                Row {
-                    Icon(imageVector = Icons.Default.Circle, contentDescription = "")
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        modifier = Modifier.size(12.dp),
+                        imageVector = Icons.Default.Circle,
+                        contentDescription = ""
+                    )
                     Spacer(modifier = Modifier.padding(8.dp))
-                    Text(text = "TEmpat")
+                    Text(text = "Delivery type")
                 }
-                Row {
-                    Icon(imageVector = Icons.Default.Circle, contentDescription = "")
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        modifier = Modifier.size(12.dp),
+                        imageVector = Icons.Default.Circle,
+                        contentDescription = ""
+                    )
                     Spacer(modifier = Modifier.padding(8.dp))
-                    Text(text = "TEmpat")
+                    Text(text = "Price")
                 }
             }
         },
         trailingContent = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Default.Delete, contentDescription = "")
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Outlined.AddCircleOutline, contentDescription = "")
+                }
+                Text(text = "0")
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Outlined.RemoveCircleOutline, contentDescription = "")
+                }
             }
         }
     )
